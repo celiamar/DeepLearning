e890d969634a0f1a78533511c31c69a74151ca8d
ehsan <ehsan@ehsans-MBP.lan>
2018-04-22 10:13:48 +0200

    Ex0: circle implemented

--- Exercise0/circle.py
+++ Exercise0/circle.py
@@ -1,9 +1,32 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-class Circle(object):
+class Circle:
+
+
+    def __init__(self, resolution, radius, position):
+        self.resolution = resolution
+        self.radius = radius
+        self.position = position
+        self.output = None
+
 
     def draw(self):
-        return 0
+        x_indicies = np.arange(self.resolution)
+        x_indicies = np.tile(x_indicies, (self.resolution, 1))
+
+        y_indicies = x_indicies.T
+
+        pos_y = (int) (self.position / self.resolution)
+        pos_x = self.position - (pos_y * self.resolution)
+
+        x_indicies = np.power((x_indicies - pos_x),2)
+        y_indicies = np.power((y_indicies - pos_y),2)
+
+        self.output = x_indicies + y_indicies
+        self.output = self.output <= self.radius ** 2
+
     def show(self):
-        return 0
+        plt.axis('off')
+        plt.imshow(self.output, cmap='gray')
+        plt.show()
--- Exercise0/main.py
+++ Exercise0/main.py
@@ -2,16 +2,21 @@ import numpy as np
 import matplotlib.pyplot as plt
 import Exercise0.checkers as ch
 import Exercise0.spectrum as sp
-
+import Exercise0.circle as cr
 
 def main():
     # checkboard = ch.Checkers(2, 16)
     # checkboard.draw()
     # checkboard.show()
 
-    spectrum = sp.Spectrum(900)
-    spectrum.draw()
-    spectrum.show()
+    # spectrum = sp.Spectrum(900)
+    # spectrum.draw()
+    # spectrum.show()
+
+    circle = cr.Circle(1000,100,555500)
+    circle.draw()
+    circle.show()
+
 if __name__== "__main__":
     main()
 
