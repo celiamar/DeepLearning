19169d52a329899f21d3f010aa0ab09c6c80ccba
ehsan <ehsan@ehsans-MBP.lan>
2018-04-22 09:31:25 +0200

    spectrum initiated

--- Exercise0/main.py
+++ Exercise0/main.py
@@ -1,14 +1,17 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import Exercise0.checkers as ch
+import Exercise0.spectrum as sp
 
 
 def main():
-    checkboard = ch.Checkers(2, 16)
-    checkboard.draw()
-    checkboard.show()
+    # checkboard = ch.Checkers(2, 16)
+    # checkboard.draw()
+    # checkboard.show()
 
-
+    spectrum = sp.Spectrum(900)
+    spectrum.draw()
+    spectrum.show()
 if __name__== "__main__":
     main()
 
--- Exercise0/spectrum.py
+++ Exercise0/spectrum.py
@@ -1,11 +1,30 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-class Spectrum(object):
+class Spectrum:
 
-
+    def __init__(self, resolution):
+        self.resolution  = resolution
+        self.output = None
 
     def draw(self):
-        return 0
+
+
+        range = np.linspace(0,self.resolution,self.resolution)/self.resolution
+        range = np.tile(range, (self.resolution,1))
+
+        r = range
+        g = range.T
+        b = np.flip(range, axis = 1)
+
+        r = np.expand_dims(r, axis=2)
+        g = np.expand_dims(g, axis=2)
+        b = np.expand_dims(b, axis=2)
+
+        self.output = np.concatenate([r, g, b], axis=2)
+
     def show(self):
-        return 0
+        print(self.output)
+        img = self.output
+        plt.imshow(img)
+        plt.show()
